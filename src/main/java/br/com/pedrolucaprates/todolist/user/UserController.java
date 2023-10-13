b5c4dd88e9f423e13fb289b797cfff35a3bc8af1
pedroluca <pedrolucaofc@outlook.com>
2023-10-13 16:41:08 -0300

    third class completed, database conected, user and password verification going on

--- src/main/java/br/com/pedrolucaprates/todolist/user/UserController.java
+++ src/main/java/br/com/pedrolucaprates/todolist/user/UserController.java
@@ -1,9 +1,14 @@
 package br.com.pedrolucaprates.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 /**
  * Modificadores:
@@ -14,10 +19,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+  @Autowired
+  private IUserRepository userRepository;
   
   @PostMapping("/")
-  public void create(@RequestBody UserModel userModel) {
-    System.out.println(userModel.name);
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if (user != null) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+    
+    var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+    userModel.setPassword(passwordHashed);
+    
+    var userCreated = this.userRepository.save(userModel);
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
   
 }
